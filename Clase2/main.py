5624992c4328b97437af58fc5bfc3beb8ccd0084
Elder Pum <ElderPum@gmail.com>
2023-08-05 08:57:28 -0600

    Ejemplo Clase 2

---
+++ Clase2/main.py
@@ -0,0 +1,16 @@
+listaPrueba = []
+listaPrueba.append(1)
+listaPrueba.append('Hola, se puede agregar un string a esta lista')
+listaPrueba.append(True)
+listaPrueba.append(3.1416)
+
+len(listaPrueba)
+
+print(listaPrueba)
+print('La longitud de la lista es de: ' + str(len(listaPrueba)))
+
+diccionarioPrueba = {}
+diccionarioPrueba['nombre'] = 'Juan'
+diccionarioPrueba['apellido'] = 'Perez'
+
+print(diccionarioPrueba)
